ba5621c8250d1ddb2f5977347579d201a479b349
Stefan Berkner <stefan.berkner@tilotech.io>
2024-09-27 13:57:05 +0200

    Add PDF processing

--- langchain_tilores/tilores_tools.py
+++ langchain_tilores/tilores_tools.py
@@ -21,15 +21,29 @@ class TiloresTools:
 
     def all(self):
         return [
+            # self.record_fields_tool,
             self.search_tool
         ]
+    
+    # def record_fields_tool(self):
+    #     return StructuredTool.from_function(**{
+    #         'name': 'tilores_record_fields',
+    #         'description': 'useful for when you need to know which fields can be queried on an entity record',
+    #         'return_direct': True,
+    #         'func': static_value(self.tilores_api.record_field_names)
+    #     })
 
     def search_tool(self):
         return StructuredTool.from_function(**{
             'name': 'tilores_search',
-            'description': 'useful for when you need to search entities on variable parameters',
+            'description': 'useful for when you need to search one or more entities; each entity is a list of records with varying information which refer to the same real world entity',
             'args_schema': self.references['SearchParams'],
             'return_direct': True,
             'func': self.tilores_api.search
         })
 
+def static_value(val):
+    def wrapper():
+        print("requested values")
+        return val
+    return wrapper
