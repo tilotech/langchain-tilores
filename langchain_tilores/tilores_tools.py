cd45cddc713a9e11e64eeac2133e4c3eeef3b1cc
hnehnes <103562534+hnehnes@users.noreply.github.com>
2024-10-09 15:09:41 +0200

    Update tilores_tools.py
    updated the search_tool description so that the LLM understands that an entity refers to a natural person.

--- langchain_tilores/tilores_tools.py
+++ langchain_tilores/tilores_tools.py
@@ -28,7 +28,7 @@ class TiloresTools:
     def search_tool(self):
         return StructuredTool.from_function(**{
             'name': 'tilores_search',
-            'description': 'useful for when you need to search one or more entities; each entity is a list of records with varying information which refer to the same real world entity',
+            'description': 'useful for when you need to search one or more entities; each entity represents one unique person and has a list of records with varying information referring to that one person; if more than one entity is returned, tell the user that there are several persons found and ask which one he is looking for.',
             'args_schema': self.references['SearchParams'],
             'return_direct': True,
             'func': self.tilores_api.search
